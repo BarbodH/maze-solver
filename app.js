37cb64e489aef4809daf2af727be2a609053091f
BarbodH <habibi.k.barbod@gmail.com>
2023-03-21 19:31:08 -0400

    Allowed user to specify a valid port number (optional)

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ import * as http from "node:http";
 import * as path from "node:path";
 import * as fileSystem from "node:fs";
 
-const PORT = 8000;
+// allow user to specify port number within terminal (optional); default port is 8000
+let arg;
+if (process.argv.length > 3) {
+  console.log("Error: too many command-line arguments! (max = 1)");
+  process.exit();
+} else if (process.argv.length == 3) {
+  arg = Number(process.argv[2]);
+  if (!Number.isInteger(arg)) {
+    console.log("Error: command-line argument (port #) must be an integer!");
+    process.exit();
+  } else if (arg < 0 || arg > 65535) {
+    console.log("Error: command-line argument (port #) must be within an appropriate range! [0, 65535]");
+    process.exit();
+  }
+} else {
+  arg = 8000;
+}
+const PORT = arg;
 
 // media types (Multipurpose Internet Mail Extensions)
 const MIME_TYPES = {
